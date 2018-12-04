ccb101b6d76d62f80418d44629efd1e768f7a816
Ankit-Mund <ankit7mund@gmail.com>
2018-12-04 17:12:17 +0530

    Socket connection implemented for Activity Plan

---
+++ socket-connection/index.js
@@ -0,0 +1,33 @@
+const userInitiativeDao = require('../dao/user-initiative/user-initiative.dao');
+const jwt = require('jsonwebtoken');
+
+function instantiateSocket(io){
+    ioInst=io
+    io.on('connection', function(socket){
+        let decoded = jwt.decode(socket.request.cookies.jwtToken);
+        let tempObj = {
+            "email": decoded.token.email
+        }
+        userInitiativeDao.getUserInitiative(tempObj).then(function(result){
+            result.initiative.map(eachInitiative => {
+                console.log(eachInitiative.initiativeId,"ID........")
+                joinRoom(eachInitiative.initiativeId,socket)
+            })
+        })
+        socket.on('message', function(data){
+            // console.log(data, "event emitted")
+            // socket.emit('message',data)
+            console.log(socket.adapter.rooms,"ROOMS.....")
+            io.in(data.initiativeId).emit('message', data)
+        })
+    })
+}
+
+function joinRoom(initiativeName,socket){
+    socket.join(initiativeName)
+    console.log(socket.adapter.rooms,"JOINED")
+}
+
+module.exports = {
+    instantiateSocket, joinRoom
+}
