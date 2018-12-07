1f599dc3d85a1c9690f9c6f2eebf755c39d072ca
ruchik.gaikwad <ruchik.gaikwad@stackroute.in>
2018-12-08 03:54:10 +0530

    test ci/cd automation

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const ENV = require('./config/environment');
 /**
  * Not to use cors in the PRODUCTION
  */
-if (process.ENV === 'dev') {
+if (process.env.NODE_ENV === 'dev') {
     app.use(function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -32,7 +32,7 @@ if (process.ENV === 'dev') {
     }); 
 }
 // console.log(path.resolve(__dirname , '../' , 'agiler-ui/dist/'))
-if(process.ENV === 'prod'){
+if(process.env.NODE_ENV === 'prod'){
     app.use('/', require('express').static(path.resolve(__dirname ,'../agiler-ui','./dist')));
 }
 io.use(parser())
